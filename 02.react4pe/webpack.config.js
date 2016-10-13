312e459ac72666b42d4ea7b6334b19ce8486939c
dyf <280956028@qq.com>
2016-10-13 21:54:20 +0800

    修改

--- 02.react4pe/webpack.config.js
+++ 02.react4pe/webpack.config.js
@@ -2,104 +2,9 @@ var appName = "csmobile";
 
 var mode = "release";
 
-mode = "debug";
+//mode = "debug";
 
 const apps = {
-    "cspc":{
-        entry:"./project/WEAVER_CloudStore/src4js/pc1.0/"+(mode=="debug"?"debug":"index")+".js",
-        output:"./project/WEAVER_CloudStore/WebRoot/WEB-INF/views/js/index.js",
-        styleUrl:mode=="debug"?"":"./project/WEAVER_CloudStore/WebRoot/WEB-INF/views/css/index.css"
-    },
-    "csmobile":{
-        entry:"./project/WEAVER_CloudStore/src4js/mobile1.0/"+(mode=="debug"?"debug":"index")+".js",
-        output:"./project/WEAVER_CloudStore/WebRoot/WEB-INF/views/m/js/index.js",
-        styleUrl:mode=="debug"?"":"./project/WEAVER_CloudStore/WebRoot/WEB-INF/views/m/css/index.css"
-    },
-    "csclient":{
-        entry:"./project/WEAVER_CloudStore_ec/src4js/cloudstore_ec/index.js",
-        output:"./project/WEAVER_CloudStore_ec/WebRoot/ecology/cloudstore/system/js/index.js"
-    },
-    "csclient4ec8":{
-        entry:"./project/WEAVER_CloudStore_ec/src4js/cloudstore_ec/index4ec8.js",
-        output:"./project/WEAVER_CloudStore_ec/WebRoot/ecology/cloudstore/system/js/index4ec8.js"
-    },
-    "comv1":{
-        entry:"./project/WEAVER_CloudStore_ec/src4js/componentsV1/index.js",
-        output:"./project/WEAVER_CloudStore_ec/WebRoot/ecology/cloudstore/resource/pc/com/index.js",
-        outputlib:{
-            library:"weaCom",
-            libraryTarget: "umd"
-        },
-        styleUrl:"./project/WEAVER_CloudStore_ec/WebRoot/ecology/cloudstore/resource/pc/antd/antd.min.css"
-    },
-    "plugin":{
-        entry:"./project/WEAVER_CloudStore_ec/src4js/cloudstore_ec/plugin.js",
-        output:"./project/WEAVER_CloudStore_ec/WebRoot/ecology/cloudstore/system/js/plugin.js",
-        outputlib:{
-            library:"weaPlugin",
-            libraryTarget: "umd"
-        }
-    },
-    "no002":{
-        entry:"./project/APP0000002/src4js/pc/index.js",
-        output:"./project/APP0000002/WebRoot/ecology/cloudstore/app/no0000002/app/js/index.js"
-    },
-    "no004":{
-        entry:"./project/APP0000004/src4js/index.js",
-        output:"./project/APP0000004/WebRoot/ecology/cloudstore/app/no0000004/js/index.js"
-    },
-    "no005":{ //工资条
-        entry:"./project/APP0000005/src4js/index.js",
-        output:"./project/APP0000005/WebRoot/ecology/cloudstore/app/no0000005/js/index.js"
-    },
-    "no006":{
-        entry:"./project/APP0000006/src4js/wfTrigger.js",
-        output:"./project/APP0000006/WebRoot/ecology/cloudstore/app/no0000006/js/wfTrigger.js"
-    },
-    "no006set":{
-        entry:"./project/APP0000006/src4js/wfTriggerSet.js",
-        output:"./project/APP0000006/WebRoot/ecology/cloudstore/app/no0000006/js/wfTriggerSet.js"
-    },
-    "no012":{
-        entry:"./project/APP0000012/src4js/index.js",
-        output:"./project/APP0000012/WebRoot/ecology/cloudstore/app/no0000012/js/wfDetail.js",
-        outputlib:{
-            library:"wfDetail",
-            libraryTarget: "umd"
-        }
-    },
-    "no012set":{
-        entry:"./project/APP0000012/src4js/set/index.js",
-        output:"./project/APP0000012/WebRoot/ecology/cloudstore/app/no0000012/js/set.js"
-    },
-    "no037":{
-        entry:"./project/APP0000037/src4js/index.js",
-        output:"./project/APP0000037/WebRoot/ecology/cloudstore/app/no0000037/js/index.js"
-    },
-    "no045":{
-        entry:"./project/APP0000045/src4js/index.js",
-        output:"./project/APP0000045/WebRoot/ecology/cloudstore/app/no0000045/js/index.js"
-    },
-    "no053":{
-        entry:"./project/APP0000053/src4js/index.js",
-        output:"./project/APP0000053/WebRoot/ecology/cloudstore/app/no0000053/js/index.js"
-    },
-    "no071":{
-        entry:"./project/APP0000071/src4js/index.js",
-        output:"./project/APP0000071/WebRoot/ecology/cloudstore/app/no0000071/js/index.js"
-    },
-    "no086":{
-        entry:"./project/APP0000086/src4js/index.js",
-        output:"./project/APP0000086/WebRoot/ecology/cloudstore/app/no0000086/js/index.js"
-    },
-    "no087":{
-        entry:"./project/APP0000087/src4js/index.js",
-        output:"./project/APP0000087/WebRoot/ecology/cloudstore/app/no0000087/js/index.js"
-    },
-    "no089":{
-        entry:"./project/APP0000089/src4js/index.js",
-        output:"./project/APP0000089/WebRoot/ecology/cloudstore/app/no0000089/js/index.js"
-    },
     "esearch":{
         entry:"./project/EC_FULLSEARCH/src4js/index.js",
         output:"./project/EC_FULLSEARCH/WebRoot/ecology/mobile/plugin/fullsearch/js/index.js",
@@ -129,7 +34,7 @@ var wp4ec = {
           allChunks: true, 
       }),
       new webpack.DefinePlugin({
-          'process.env.NODE_ENV':"'development'" //production
+          'process.env.NODE_ENV':"'production'" //production
       })
     ],
     externals: [
