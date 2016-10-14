85379a6a0b070e2b758be958f9226875af3875fe
Yvan <daiyingfeng@hotmail.com>
2016-10-14 17:53:52 +0800

    Update webpack.config.js

--- 02.react4pe/webpack.config.js
+++ 02.react4pe/webpack.config.js
@@ -47,7 +47,7 @@ var wp4ec = {
     module: {
       loaders: [
           { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-          { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
+          { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!postcss-loader") },
           { test: /\.jpe?g$|\.gif$|\.eot$|\.png$|\.svg$|\.woff$|\.woff2$|\.ttf$/,loader: "file" },
           { test: /\.less$/,loader: ExtractTextPlugin.extract('css!less') }
       ]
