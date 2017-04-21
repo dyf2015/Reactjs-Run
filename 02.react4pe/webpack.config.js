720fd064e88bcfd93c1b9d720a5403865f61e21f
daiyingfeng <280956028@qq.com>
2017-04-21 15:47:24 +0800

    提交

--- 02.react4pe/webpack.config.js
+++ 02.react4pe/webpack.config.js
@@ -1,21 +1,19 @@
 var config = require('./weaconfig');
 
-var appName = "no005";
+var appName = "demo1";
 
 var mode = "release";
 
 var node_env = "production";
 
-var isOutput2Custom = true; //是否输出到自定义路径，开启后原路径将不输出
-
-var customPath = "./DEMO/Weaver_20170116"; //自定义路径地址
-
 const apps = {
-    "no005":{ //一个项目
-        entry:"./project/APP0000005/src4js/index.js", //项目路径
-        output:"./project/APP0000005/WebRoot/ecology/cloudstore/app/no0000005/js/index.js",
-        output2Custom:customPath+"/ecology/cloudstore/app/no0000005/js/index.js",
-        isOutput2Custom:isOutput2Custom
+    "demo1":{ //一个项目
+        entry:"./project/demo1/src4js/index.js", //项目路径
+        output:"./project/demo1/WebRoot/ecology/cloudstore/app/no0000005/js/index.js"
+    },
+    "demo2":{ //一个项目
+        entry:"./project/demo2/src4js/index.js", //项目路径
+        output:"./project/demo2/WebRoot/ecology/cloudstore/app/no0000005/js/index.js"
     }
 }
 
